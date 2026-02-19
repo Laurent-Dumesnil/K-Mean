cac3801021f9d599faeaf6b58ef608849d89bf0e
Jérémie Proulx <djepro4@gmail.com>
2026-02-19 11:52:42 -0500

    Added constructor and verifictation for the Prediction class

--- tp1/Solution_Julien/main.py
+++ tp1/Solution_Julien/main.py
@@ -15,16 +15,16 @@ def main():
     print(train.matrix)
     exit = False
     while not exit:
-        answers = input("Entrez un mot, le nombre de synonymes que vous voulez et la méthode de calcul, i.e. produit scalaire: 0, least-squares: 1, city-block:2\nTapez q pour quitter\n\n").split(" ")
+        answers = input("Entrez un mot, le nombre de synonymes que vous voulez et la méthode de calcul, i.e. produit scalaire: 0, least-squares: 1, city-block:2\nTapez q pour quitter\n\n").strip().lower().split(" ")
         if answers[0] == "q":
             print("AU REVOIR")
             return 0
         elif len(answers) != 3:
             raise ValueError('Vous devez écrire selon ce pattern: "mot_recherché" "nombre_entier" "nombre_0_1-2"')
-        results = Prediction(train, answers[0], answers[1], answers[2])
+        prediction = Prediction(train, answers[0], answers[1], answers[2])
 
-        for keys, values in results.items():
-            print(keys + " --> " + values)
+        for key, value in prediction.result.items():
+            print(f'{key} --> {value}')
     
 if __name__ == '__main__':
     quit(main())
