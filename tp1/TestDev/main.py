68714aeadb4de5cfe621c6e651e1d54e7c86ecb8
Julien Lamontagne <e.jlamontagne3@etu.cvm.qc.ca>
2026-03-05 09:29:01 -0500

    ajout du dossier test

---
+++ tp1/TestDev/main.py
@@ -0,0 +1,30 @@
+from sys import argv
+from training import Training
+from prediction import Prediction
+
+def main():
+    # window = argv[1]
+    # file = argv[2]
+    # encode =  argv[3]
+
+    window = 5
+    file = "C:/travail/GerminalUTF8.txt"
+    encode = "UTF-8"
+    
+    train = Training(window, file, encode)
+    print(train.matrix)
+    exit = False
+    while not exit:
+        answers = input("Entrez un mot, le nombre de synonymes que vous voulez et la méthode de calcul, i.e. produit scalaire: 1, least-squares: 2, city-block:3\nTapez q pour quitter\n\n").strip().lower().split(" ")
+        if answers[0] == "q":
+            print("AU REVOIR")
+            return 0
+        elif len(answers) != 3:
+            raise ValueError('Vous devez écrire selon ce pattern: "mot_recherché" "nombre_entier" "nombre_0_1-2"')
+        prediction = Prediction(train, answers[0], answers[1], answers[2])
+
+        for key, value in prediction.result.items():
+            print(f'{key} --> {value}')
+    
+if __name__ == '__main__':
+    quit(main())
---
+++ tp1/TestDev/training.py
@@ -0,0 +1,108 @@
+from sys import argv
+import re
+import numpy as np
+
+class Training():
+        def __init__(self, window, file, encode):
+                try:
+                        self.text = self.read_text(file, encode)
+                        self.words = re.findall(r"\b\w+\b", self.text.lower())
+                except Exception:
+                        print("Erreur lors de l'ouverture du fichier")
+
+                self.dict = self.create_dict(self.words)
+                self.matrix = self._create_matrix(self.dict)
+                self.fill_matrix_vector(self.matrix, self.words, self.dict, window)
+                self.lookup_table = self.create_lookup_table()
+
+        def create_lookup_table(self):
+                lookup = []
+                for keys, _ in self.dict.items():
+                        lookup.append(keys)
+                return lookup
+
+        def read_text(self, file, encode):
+                f = open(file, encoding=encode)
+                text = f.read()
+                f.close()
+
+                return text
+        
+        def create_dict(self, words):
+                d =  dict.fromkeys(words)
+                index = 0
+
+                for index, key in enumerate(d):
+                        d[key] = index
+                return d
+
+        def _create_matrix(self, dict):
+                size = len(dict)
+                ndarray = np.array(np.zeros((size, size)))
+                print(np.shape(ndarray))
+
+                return ndarray
+        
+        def fill_matrix_vector(self, matrix, words, dict, window):
+                neighbors_qty = window//2
+
+                #on parcours chaque mot du texte
+                for i in range(len(words)):
+                        #On trouve l'index de ce mot dans le dict
+                        index = dict[words[i]]
+                        #On trouve les voisins avant
+                        count = -1
+                        for _ in range(1, neighbors_qty+1):
+                                #Empecher le outofbound
+                                if i+count < 0:
+                                        count -= 1
+                                        continue
+
+                                neighbor = words[i+count]
+                                idx = dict[neighbor]
+                                matrix[index, idx] += 1
+                                count -= 1
+
+                        #On trouve les voisins après
+                        count = 1
+                        for _ in range(1, neighbors_qty+1):
+                                #Empecher le outofbound
+                                if i+count >= len(words):
+                                        count += 1
+                                        continue
+
+                                neighbor = words[i+count]
+                                idx = dict[neighbor]
+                                matrix[index, idx] += 1
+                                count += 1
+
+                return matrix
+
+        def fill_matrix_vector_v2(self, matrix, words, dict, window):
+                neighbors_qty = window//2
+                n = len(words)
+
+                #on parcours chaque mot du texte
+                for i in range(n):
+                        #On trouve l'index de ce mot dans le dict
+                        index = dict[words[i]]
+                        #On trouve les voisins avant
+                        for offset in range(-neighbors_qty, neighbors_qty+1):
+                                #Empecher le outofbound
+                                if offset == 0:
+                                        continue
+
+                                j = i + offset
+
+                        #On trouve les voisins après
+                                if 0 <= j < n:
+                                        neighbor = words[j]
+                                        idx = dict[neighbor]
+                                        matrix[index, idx] += 1
+                return matrix
+
+
+
+
+
+        
