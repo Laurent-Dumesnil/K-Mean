a34b573d781760ff73b6a60db726579425a5aeb0
Jérémie Proulx <djepro4@gmail.com>
2026-03-12 11:48:42 -0400

    Base TP2

---
+++ TP2/src/main.py
@@ -0,0 +1,70 @@
+from sys import argv
+from time import perf_counter
+from traceback import print_exc
+from entrainer import Entrainer
+from predire import Predire
+
+PRINT_TIME = 1
+PRINT_ALL = 2
+
+QUITTER = 'q'
+
+INVITE = f"""
+Entrez un mot, le nombre de synonymes que vous voulez et la méthode de calcul,
+i.e. produit scalaire: 0, least-squares: 1, city-block: 2
+
+Tapez {QUITTER} pour quitter.
+
+"""
+
+
+def demande_utilisateur(cerveau: Entrainer, verbose: int) -> None:
+    reponse = input(INVITE)
+    while reponse != QUITTER:
+        try:
+            mot, nb, methode = reponse.split()
+            nb, methode = int(nb), int(methode)
+            print()
+            t = perf_counter()
+            resultats = Predire.prediction(cerveau, mot, nb, methode)
+            duree = perf_counter() - t
+            for mot, score in resultats:
+                print(f'{mot} --> {score}')
+            if verbose >= PRINT_TIME:
+                print(f'\nPrédiction effectuée en {duree: .2f} secondes.')
+
+        except Exception as e:
+            print(e)
+        reponse = input(INVITE)
+
+def main() -> int:
+    
+    try:
+        taille_fenetre = int(argv[1])
+        encodage, chemin = argv[2:4]
+        if len(argv) > 4:
+            verbose = int(argv[4])
+        else:
+            verbose = 0
+
+        cerveau = Entrainer(taille_fenetre)
+        t = perf_counter()
+        cerveau.entraine(chemin, encodage)
+        if verbose >= PRINT_TIME:
+            print(f'Entraînement effectué en {perf_counter() - t:.2f} secondes.')
+            if verbose >= PRINT_ALL:
+                print(cerveau)
+        demande_utilisateur(cerveau, verbose)
+
+    except ValueError as ve:
+        print(ve)
+        return -2
+    except Exception as e:
+        # print_exc()
+        print(e)
+        return -1
+
+    return 0
+
+if __name__ == '__main__':
+    quit(main())
---
+++ TP2/src/predire.py
@@ -0,0 +1,32 @@
+import numpy as np
+import numpy.typing as npt
+from entrainer import Entrainer
+
+STOP = set('l le la les au aux un une du des c ça ce cette cet ces celle celui celles ceux je j tu il elle on nous vous ils elles me m te t se s y à de pour sans par mais ou et donc car ni or ne n pas dans que qui qu de d mon ma mes ton ta tes son sa ses notre nos votre vos leur leurs lui en quel quelle quelles lequel laquelle lesquels lesquelles dont quoi quand où comment pourquoi sur dessus tout tous toutes avec comme avec'.split())
+
+def ls(u: npt.NDArray[np.float64], v: npt.NDArray[np.float64]) -> np.float64:
+    return np.sum((u-v)**2)
+
+def cb(u: npt.NDArray[np.float64], v: npt.NDArray[np.float64]) -> np.float64:
+    return np.sum(np.abs(u-v))
+
+class Predire():
+
+    fonctions = [np.dot, ls, cb]
+
+    @staticmethod
+    def prediction(cerveau: Entrainer, mot_recherche: str, nb: int, methode: int) -> list[tuple[str, np.float64]]:
+        if mot_recherche not in cerveau.vocabulaire:
+            raise Exception(f'"{mot_recherche}" n\'est pas dans le vocabulaire.')
+        
+        index_recherche = cerveau.vocabulaire[mot_recherche]
+        v = cerveau.matrice[index_recherche]
+        fonction = Predire.fonctions[methode]
+        resultats = []
+        for mot, index in cerveau.vocabulaire.items():
+            if index != index_recherche and mot not in STOP:
+                resultats.append( (mot, fonction(v, cerveau.matrice[index])) )
+        return sorted(resultats, key=lambda t:t[1], reverse = fonction == np.dot)[:nb]
+
+        
+        
